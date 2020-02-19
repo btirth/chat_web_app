95e155876f489f3c826d3a25a7170701a9ee419d
Tirth Bharatiya <emailtirth@gmail.com>
2020-02-19 23:25:25 +0530

    Real time chat web application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const port = process.PORT || 3000
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New Connection')
+})
+
+server.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
